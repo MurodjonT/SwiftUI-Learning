3b54be2facd78b045573bd44c6a2ce6bc925381f
MurodjonT <murodjon.mmm.99@mail.ru>
2025-05-01 10:58:22 +0500

    Initial Commit

---
+++ SwiftUI Learning/SwiftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LearningApp.swift
+//  SwiftUI Learning
+//
+//  Created by Murodjon Turobov on 01/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
